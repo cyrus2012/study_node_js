bbd849258b9a28170b864134f7b74ef538127da0
cy <cy@example.com>
2026-01-08 09:20:04 +0800

    apply passport and local strategy. able to redirect page when authentication success

---
+++ usePassport_with_local_Strategy/index.js
@@ -0,0 +1,67 @@
+import express from "express";
+import bodyParser from "body-parser";
+import passport from "passport";
+import {Strategy} from "passport-local";
+
+const app = express();
+
+const PORT = 6000;
+
+app.use(bodyParser.urlencoded({extends:false}));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) =>{
+    return res.send("this is root page. Do need authentication");
+});
+
+app.post(
+    "/login", 
+    passport.authenticate(
+        "local", 
+        {
+            successRedirect: "/secrets",
+            failureRedirect: "/",
+            session:false
+        }
+    )
+);
+
+app.get("/secrets", (req, res) =>{
+   return res.send("This is secrets page. Access this page after login");
+});
+
+app.listen(PORT, () => 
+    console.log(`start listening to port ${PORT}`)
+);
+
+
+const user = {name: "Tom", password:"123456"};
+
+function getUserInfo(username){
+    if(username === user.name)
+        return user;
+    return null;
+}
+
+passport.use(
+    "local", 
+    new Strategy(
+        {usernameField: "username", passwordField:"password"}, 
+        function verify(username, password, cb){
+            const userCredentials = getUserInfo(username);
+            if(!userCredentials)
+                return cb("User does not exist", false);
+            if(password === userCredentials.password){
+                const userInfo = {};
+                userInfo.name = userCredentials.name;
+                //only provide the necessary info to callback function
+                return cb(null, userInfo);
+            }
+
+            return cb("incorrect password", null);
+
+        }
+    )
+);
+
+
